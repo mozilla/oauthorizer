e16b370fd5734b675643a03827c55a81cee2220e
Shane Caraveo <mixedpuppy@gmail.com>
2010-05-01 15:40:40 -0700

    add a progress bar for the loading of the oauth web pages

--- content/loginPanel.js
+++ content/loginPanel.js
@@ -1,4 +1,6 @@
 
+const wpl = Components.interfaces.nsIWebProgressListener;
+
 function closeWindow ()
 {
   window.arguments[1].call(null, null);		
@@ -7,13 +9,28 @@ function closeWindow ()
 
 function doneAuthorizing(oauth_verifier)
 {
-  dump("doneAuthorizing: "+oauth_verifier+"\n");
   window.arguments[1].call(null, oauth_verifier);		
   window.close();
 }
-              
-var reporterListener = {
 
+var reporterListener = {
+  _isBusy: false,
+  get statusMeter() {
+    delete this.statusMeter;
+    return this.statusMeter = document.getElementById("statusbar-icon");
+  },
+  get securityButton() {
+    delete this.securityButton;
+    return this.securityButton = document.getElementById("security-button");
+  },
+  get securityLabel() {
+    delete this.securityLabel;
+    return this.securityLabel = document.getElementById("security-status");
+  },
+  get securityDisplay() {
+    delete this.securityDisplay;
+    return this.securityDisplay = document.getElementById("security-display");
+  },
   QueryInterface: function(aIID) {
     if (aIID.equals(Components.interfaces.nsIWebProgressListener)   ||
         aIID.equals(Components.interfaces.nsIWebProgressListener2)  ||
@@ -26,7 +43,18 @@ var reporterListener = {
                      /*in nsIRequest*/ aRequest,
                      /*in unsigned long*/ aStateFlags,
                      /*in nsresult*/ aStatus) {
-    },
+    if (aStateFlags & wpl.STATE_START &&
+        aStateFlags & wpl.STATE_IS_NETWORK) {
+      this.statusMeter.value = 0;
+      this.statusMeter.parentNode.collapsed = false;
+      this.securityLabel.collapsed = true;
+    }
+    else if (aStateFlags & wpl.STATE_STOP &&
+             aStateFlags & wpl.STATE_IS_NETWORK) {
+      this.statusMeter.parentNode.collapsed = true;
+      this.securityLabel.collapsed = false;
+    }
+  },
 
   onProgressChange: function(/*in nsIWebProgress*/ aWebProgress,
                         /*in nsIRequest*/ aRequest,
@@ -34,51 +62,47 @@ var reporterListener = {
                         /*in long */aMaxSelfProgress,
                         /*in long */aCurTotalProgress,
                         /*in long */aMaxTotalProgress) {
-    },
+    if (aMaxTotalProgress > 0) {
+      let percentage = (aCurTotalProgress * 100) / aMaxTotalProgress;
+      this.statusMeter.value = percentage;
+    }
+  },
 
   onLocationChange: function(/*in nsIWebProgress*/ aWebProgress,
                         /*in nsIRequest*/ aRequest,
                         /*in nsIURI*/ aLocation) {
     // XXX this needs to be cleaned up to handle differences better, the
     // callback url should be configurable as well
-    dump("onLocationChange: "+aLocation.spec+"\n");
-      document.getElementById('security-display').setAttribute('label', aLocation.host);
-      if (aLocation.host.indexOf('oauthcallback.local') >= 0) {
-        // OAuth 1.0
-        var oauth_verifier = /oauth_verifier=([^&]*)/gi.exec(aLocation.spec);
-        if (oauth_verifier) {
-          dump("***** oauth_verifier: "+oauth_verifier[1]+"\n");
-          doneAuthorizing(oauth_verifier[1]);
-        }
-        // OAuth 2.0
-        var oauth_code = /#access_token=([^&]*)/gi.exec(aLocation.spec);
-        if (oauth_code) {
-          dump("***** oauth_code: "+oauth_code[1]+"\n");
-          doneAuthorizing(oauth_code[1]);
-        }
+    this.securityDisplay.setAttribute('label', aLocation.host);
+    if (aLocation.host.indexOf('oauthcallback.local') >= 0) {
+      // OAuth 1.0
+      var oauth_verifier = /oauth_verifier=([^&]*)/gi.exec(aLocation.spec);
+      if (oauth_verifier) {
+        doneAuthorizing(oauth_verifier[1]);
       }
-    },
+      // OAuth 2.0
+      var oauth_code = /#access_token=([^&]*)/gi.exec(aLocation.spec);
+      if (oauth_code) {
+        doneAuthorizing(oauth_code[1]);
+      }
+    }
+  },
 
   onStatusChange: function(/*in nsIWebProgress*/ aWebProgress,
                       /*in nsIRequest*/ aRequest,
                       /*in nsresult*/ aStatus,
                       /*in wstring*/ aMessage) {
-    },
+  },
 
   onSecurityChange: function(/*in nsIWebProgress*/ aWebProgress,
                         /*in nsIRequest*/ aRequest,
                         /*in unsigned long*/ aState) {
-    dump("onSecurityChange: ["+aRequest.name+"] state "+aState+"\n");
-try {
-    const wpl = Components.interfaces.nsIWebProgressListener;
     const wpl_security_bits = wpl.STATE_IS_SECURE |
                               wpl.STATE_IS_BROKEN |
                               wpl.STATE_IS_INSECURE |
                               wpl.STATE_SECURE_HIGH |
                               wpl.STATE_SECURE_MED |
                               wpl.STATE_SECURE_LOW;
-    let securityButton = document.getElementById('security-button');
-    let securityLabel = document.getElementById('security-status');
     var browser = document.getElementById("oauth_loginFrame");
     var level;
     
@@ -94,37 +118,35 @@ try {
         level = "broken";
         break;
     }
-    dump("security level: "+level+" "+browser.securityUI.tooltipText+"\n")
     if (level) {
-      securityButton.setAttribute("level", level);
-      securityButton.hidden = false;
-      securityLabel.setAttribute("label", browser.securityUI.tooltipText);
+      this.securityButton.setAttribute("level", level);
+      this.securityButton.hidden = false;
+      this.securityLabel.setAttribute("label", browser.securityUI.tooltipText);
     } else {
-      securityButton.hidden = true;
-      securityButton.removeAttribute("level");
+      this.securityButton.hidden = true;
+      this.securityButton.removeAttribute("level");
     }
-    securityButton.setAttribute("tooltiptext", browser.securityUI.tooltipText);
-} catch (e) {
-dump(e+"\n");
-}
-    },
+    this.securityButton.setAttribute("tooltiptext", browser.securityUI.tooltipText);
+  },
   onProgressChange64: function() {
-    dump("onProgressChange64: \n");
-    },
+    return this.onProgressChange(aWebProgress, aRequest,
+      aCurSelfProgress, aMaxSelfProgress, aCurTotalProgress,
+      aMaxTotalProgress);
+  },
   onRefreshAttempted: function() {
-    dump("onRefreshAttempted: \n");
-    return true; }
+    return true;
+  }
 }
 
 
 function loadLoginFrame()
 {
-  let nsIWebProgress = Components.interfaces.nsIWebProgress;
   var browser = document.getElementById("oauth_loginFrame");
-  browser.addProgressListener(reporterListener, nsIWebProgress.NOTIFY_ALL);
+  browser.addProgressListener(reporterListener, wpl.NOTIFY_ALL);
   var url = window.arguments[0];
-  if (url != "")
+  if (url != "") {
     window.setTimeout(function(url) {
           browser.setAttribute("src", url);
     }, 2000, url);
+  }
 }
